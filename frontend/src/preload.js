f32178a49212c65380e179132a66a0e4430f6e01
Mikhail <mikhailbuhavancm@gmail.com>
2025-06-09 09:47:27 +0800

    new input feature

--- frontend/src/preload.js
+++ frontend/src/preload.js
@@ -4,4 +4,7 @@ contextBridge.exposeInMainWorld('api', {
   startTracking: (task) => ipcRenderer.invoke('start-tracking', task),
   stopTracking: () => ipcRenderer.send('stop-tracking'),
   onWindowData: (callback) => ipcRenderer.on('active-window-data', (_, data) => callback(data)),
+  saveSetupData: (data) => ipcRenderer.invoke('save-setup-data', data),
+  getSetupData: () => ipcRenderer.invoke('get-setup-data'),
+  loadMainWindow: () => ipcRenderer.send('load-main-window'),
 });
